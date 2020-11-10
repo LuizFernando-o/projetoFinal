96d307ceb13a5522a8d3596440ebeb69adae7eca
luizfernando-o <luiz.2019230186@unicap.br>
2020-11-10 14:38:25 -0300

    Criação de estrutura de pastas e linkagem dos arquivos.

--- index.php
+++ index.php
@@ -1,11 +1,43 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projeto Final</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="app/assets/css/bootstrap.min.css">
+    <!-- Outros -->
+    <link rel="stylesheet" href="app/assets/img/icons/icomon.css">
+    <!-- Meu CSS -->
+    <link rel="stylesheet" href="app/assets/css/projetoFinalCSS.css">
+
 </head>
+
 <body>
-    
+
+    <!-- Header -->
+    <!-- Fim Header -->
+
+
+
+
+    <!-- Páginas do meu site -->
+    <!-- Fim Páginas do meu site -->
+
+
+
+
+    <!-- Footer -->
+
+    <!-- JQuery -->
+    <script src="app/assets/js/jquery.js"></script>
+    <!-- Bootstrap -->
+    <script src="app/assets/js.bootstrap.min.js"></script>
+    <!-- Meu JavaScript -->
+    <script src="app/assets/projetoFinal.js"></script>
+
 </body>
+
 </html>
